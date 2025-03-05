d37b652fe1d42ac6dd372d208fe884bec1bbde35
ReutAS <reutas39@gmail.com>
2025-03-05 16:15:17 +0200

    sec

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class Schedules(Base):
+    __tablename__ = 'schedules'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    doctors_stuff: Mapped[str] = mapped_column(String, nullable=False)
+    frequency: Mapped[str] = mapped_column(String, nullable=False)
+    duration = Mapped[int] = mapped_column( Integer, nullable=False)
+    recipient_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
