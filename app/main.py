b79fcc04166af2b24fd6001dd6d0b0f6bba52397
ReutAS39 <reutas39@gmail.com>
2025-03-22 09:03:28 +0200

    database ref

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ from app.models import Base
 from app.api.views import router as api_router
 from app.config import settings
 
-from app.database import db_helper
+from app.database import engine
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
     yield
